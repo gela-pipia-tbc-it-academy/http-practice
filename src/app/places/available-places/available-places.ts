0e876769523c410546c97d3e54346f6d8e91180d
Gela Pipia <gelaphiphia10@gmail.com>
2026-01-05 13:22:46 +0400

    feat: enhance error handling and loading state in available places component

--- src/app/places/available-places/available-places.ts
+++ src/app/places/available-places/available-places.ts
@@ -1,10 +1,10 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError, throwError } from 'rxjs';
 
 import { Place } from '../places.model';
-import { PlacesContainer } from '../places-container/places-container';
-import { Places } from '../places';
+import { PlacesContainer } from '../shared/places-container/places-container';
+import { Places } from '../shared/places/places';
 
 const BASE_URL = 'http://localhost:3000';
 
@@ -25,24 +25,38 @@ export class AvailablePlaces implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   isFetching = signal(false);
-  
+  error = signal<string>('');
+
   ngOnInit(): void {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<IData>(`${BASE_URL}/places`)
       .pipe(
-        map((resData) => resData.places )
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => {
+            return new Error('Something went wrong fetching available places.');
+          });
+        })
       )
       .subscribe({
         next: (places) => {
           this.places.set(places);
         },
+        error: (err: Error) => {
+          this.error.set(err.message);
+        },
         complete: () => {
           this.isFetching.set(false);
-        }
+        },
       });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
+  }
+
+  onSelectPlace(place: Place) {
+    console.log(place);
   }
 }
